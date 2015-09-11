de8b48cdb91561af6d9febefd7f04e7c6fd8e75b
Jekaterina Vorošilova <jekavor@greeny.cs.tlu.ee>
2015-09-11 11:39:04 +0300

    master

--- hello_world.php
+++ hello_world.php
@@ -1,9 +1,30 @@
 <?php
 
    $first_name = "Katrin";
+   $last_name = "Voroshilova"
 
    echo $first_name;
    
    
    
 ?>
+
+<br>
+
+<?php
+
+   $age = 5;
+   // kui on väiksem kui 18, siis alaealine
+   // kui on suurem siis täisealine
+   if($age < 18) {
+	   //tõeline
+	   echo "alaealine";
+	   
+	 } else {
+		//väärt
+        echo "täisealine"		
+	    
+	   
+	   
+   }
+
